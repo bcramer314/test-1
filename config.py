747fb78dd03c4f0c0023c5e5f1bd777107aa3410
bcramer314 <brandoncramer@Brandons-MacBook-Pro-9.local>
2020-04-03 02:10:14 -0700

    add config.py file

---
+++ config.py
@@ -0,0 +1,5 @@
+# config file
+client_ID = '06086e3b42c14b38b96993b5fe01199a'
+client_secret = '113f222f53cb4686aad6dbeb9684e7da'
+redirect_uri = 'http://localhost:7777/callback'
+scope = 'user-read-private playlist-modify-public playlist-read-private playlist-modify-private'
